1c031793aea68f52d730c357e4daa07cfdca0a53
pys <bark6256@naver.com>
2022-05-21 14:44:50 +0900

    DB모델 완성

---
+++ src/main/java/com/cos/blogapp2/domain/board/Board.java
@@ -0,0 +1,30 @@
+package com.cos.blogapp2.domain.board;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import com.cos.blogapp2.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Entity
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	// 전략 만들기
+	private int id;
+	private String title;
+	@Lob	// 이게 있어야 저장 공간이 커진다(4GB)
+	private String content;
+	
+	@ManyToOne
+	private User user;
+}
---
+++ src/main/java/com/cos/blogapp2/domain/user/User.java
@@ -0,0 +1,24 @@
+package com.cos.blogapp2.domain.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor	// 초기화 하면서 값을 집어넣을때 사용
+// @Setter	값을 변경할때 사용. DB의 값과 완벽하게 동기화가 되어야 하기 때문에 실무에서는 모델에 setter를 안쓴다. -> builder패턴
+@Getter
+@Entity	// 이게 있어야 테이블이 만들어진다
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	// 전략 만들기
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+}
