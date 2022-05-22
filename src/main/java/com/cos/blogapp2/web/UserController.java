117778f721bc2db50292f0ae8583d1a99b85871a
pys <bark6256@naver.com>
2022-05-22 20:06:49 +0900

    회원가입 완료

--- src/main/java/com/cos/blogapp2/web/UserController.java
+++ src/main/java/com/cos/blogapp2/web/UserController.java
@@ -3,9 +3,27 @@ package com.cos.blogapp2.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.blogapp2.domain.user.User;
+import com.cos.blogapp2.domain.user.UserRepository;
+import com.cos.blogapp2.web.dto.JoinReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+	private final UserRepository userRepository;
+	
+	@PostMapping("/join")
+	public String join(JoinReqDto dto) {
+		
+		User user = dto.toEntity();
+		userRepository.save(user);
+		
+		return "redirect:/loginForm";
+	}
 	
 	@GetMapping("/loginForm")
 	public String loginForm() {
---
+++ src/main/java/com/cos/blogapp2/web/dto/JoinReqDto.java
@@ -0,0 +1,23 @@
+package com.cos.blogapp2.web.dto;
+
+import com.cos.blogapp2.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class JoinReqDto {
+	private String username;
+	private String password;
+	private String email;
+	
+	public User toEntity() {
+		User user = User.builder()
+				.username(username)
+				.password(password)
+				.email(email)
+				.build();
+		return user;
+	}
+}
